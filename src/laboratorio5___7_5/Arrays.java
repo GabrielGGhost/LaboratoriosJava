eb9b76bdc566be81407ee55cf64786cd366c60b6
GabryelGGhost <gabryel_120@hotmail.com>
2020-09-23 18:01:57 -0300

    Laboratório Array feito

---
+++ src/laboratorio5___7_5/Arrays.java
@@ -0,0 +1,17 @@
+package laboratorio5___7_5;
+
+class Arrays {
+
+  public static void main(String args[]) {
+	String array[] = new String[5];
+	
+	for (int i = 0; i < array.length; i++) {
+		array[i] = "Cliente" + (i+1) + "@hotmail.com"; 
+	}
+	
+	int count = 0;
+	for (String email : array) {
+		System.out.println("Cliente " + ++count + " email: " + email);
+	}
+  }
+}
