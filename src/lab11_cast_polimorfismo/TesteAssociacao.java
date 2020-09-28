bc07c98af18f474919c60b019680247529372d1c
GabryelGGhost <gabryel_120@hotmail.com>
2020-09-28 00:48:23 -0300

    Lab11 pt1 feita

---
+++ src/lab11_cast_polimorfismo/TesteAssociacao.java
@@ -0,0 +1,23 @@
+package lab11_cast_polimorfismo;
+
+public class TesteAssociacao {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n\nPASSO1: Criacao do banco:");
+        Banco oBanco = new Banco("GlobalBank", 100);
+        oBanco.imprimeDados();
+ 
+        System.out.println("\n\nPASSO2: Criacao da agencia:");
+        Agencia ag = new Agencia("001", oBanco);
+        ag.imprimeDados();
+
+        System.out.println("\n\nPASSO3: Criacao do titular:");
+        Cliente titular = new Cliente("Joao da Silva", "12323");
+        titular.imprimeDados();
+
+        System.out.println("\n\nPASSO4: Criacao da conta:");
+        Conta aConta = new Conta(10000, "1265", titular, ag);
+        aConta.imprimeDados();
+    }
+}
